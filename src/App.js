fe40307f1d5f66292b0750cb97b40a26ecefa1ef
Deborah <liz.joy43@gmail.com>
2024-12-04 18:21:20 -0700

    added the Github repo link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Sapphire59"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Deborah Chacko
+        </a>
+      </footer>
     </div>
   );
 }
